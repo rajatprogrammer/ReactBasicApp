430105007cf1c71f539e7da9ae187997960fb5c0
rajatprogrammer <rajatgpt77@gmail.com>
2019-10-08 11:29:01 +0530

    react basic lesson completed

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './Cockpit.css';
+const cockpit = (props) => {
+    let btnClass = '';
+    if (props.showPersons) {
+        btnClass = classes.Red
+    }
+    let className = [];
+    if (props.persons.length === 1) {
+        className.push(classes.red);
+    }
+    else if (props.persons.length === 2) {
+        className.push(classes.bold);
+    }
+    else if (props.persons.length === 0) {
+        className.length = 0;
+        className.push(classes.blue);
+    }
+
+    return (
+        <div>
+            <h1>Hi, I'm a React App</h1>
+            <p className={className.join(" ")}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+        </div>
+    );
+}
+export default cockpit;
---
+++ src/containers/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Cockpit from '../components/Cockpit/Cockpit'
+import PersonsArray from '../components/persons/person';
+import Paragraph from '../components/persons/Person/paragraph';
+
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "ddsd1", name: 'Max', age: 28 },
+      { id: "2323232", name: 'Manu', age: 29 },
+      { id: "323232s", name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false,
+    length: 0
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event, id) => {
+    const persons = [...this.state.persons];
+    persons.forEach((person, index) => {
+      if (person.id === id) {
+        person.name = event.target.value;
+        persons[index] = person;
+        return false;
+      }
+    })
+    this.setState({ persons: persons });
+  }
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  }
+
+  togglePersonHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons })
+  }
+
+  paragraphLengthCalculate = (event) => {
+    this.setState({ length: event.target.value.length })
+  }
+
+  render() {
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {
+            <PersonsArray
+              persons={this.state.persons}
+              clicked={this.deletePersonHandler}
+              changed={this.nameChangedHandler}
+            />
+          }
+        </div>
+      )
+    }
+
+    let paragraphTest = (
+      <div>
+        <Paragraph
+          length={this.state.length}
+          changed={(event) => { this.paragraphLengthCalculate(event) }}
+        />
+      </div>
+    )
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          clicked={this.togglePersonHandler}
+          persons={this.state.persons}
+        />
+        {persons}
+        {paragraphTest}
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default App;

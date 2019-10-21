289cd868dd7e3de2b1adf3cee372f85676eaf2b9
rajatprogrammer <rajatgpt77@gmail.com>
2019-10-21 20:25:06 +0530

    added space

--- src/components/persons/Person/paragraph.js
+++ src/components/persons/Person/paragraph.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './Person.css';
 
-const paragraph = ( props ) => {
+const paragraph = (props) => {
     return (
         <div className="Person">
             <p >The Length of the paragaraph {props.length}</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.data}/>
+            <input type="text" onChange={props.changed} value={props.data} />
         </div>
+
     )
 };
 
